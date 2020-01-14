f364c29d51e94812e1a30983074154277f20dfa9
Jasmine Terry <jasmine.terry91@gmail.com>
2020-01-14 16:41:30 -0500

    my usercard done

--- src/UserCards.js
+++ src/UserCards.js
@@ -11,8 +11,14 @@ return (
 <div className = "user-cards">
     {props.users.map(user => (
         <div key = {user.id} className = "user">
-            <img src = {user.avatar_url} alt = "user avi"/>
+            <img width = "200" src = {user.avatar_url} alt = "user avi"/>
             <h4>{user.name}</h4>
+            <h5>Github Handle: {user.login}</h5>
+            <a href = {user.html_url} alt = "user page link">Profile</a>
+            <p>Company: {user.company}</p>
+            <p>Bio: {user.bio}</p>
+            <p>Followers: {user.followers}</p>
+            <p>Following: {user.following}</p>
         </div>
     ))}
 </div>
